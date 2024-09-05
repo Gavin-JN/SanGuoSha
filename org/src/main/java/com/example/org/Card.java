902380d20cf82ae96e1b1e81a384c6492529bb48
2561651665 <149984263+2561651665@users.noreply.github.com>
2024-09-05 13:30:50 +0800

    7

--- org/src/main/java/com/example/org/Card.java
+++ org/src/main/java/com/example/org/Card.java
@@ -31,6 +31,10 @@ public class Card {
 /*杀：出牌阶段对敌方使用，可造成一点伤害，可在对方决斗，南蛮入侵，借刀杀人时打出，
     一般情况下每回合仅可使用1张，typeId 1 */
 class Sha extends Card{
+   public Sha(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative(){
       return true;
    }
@@ -45,6 +49,10 @@ class Sha extends Card{
 
 //闪：可在对方杀，万箭齐发时打出，防止受到伤害， typeId 2
 class Shan extends Card{
+   public Shan(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanPassive(){
       return true;
    }
@@ -52,6 +60,10 @@ class Shan extends Card{
 
 //桃：可在自己回合已受伤时或濒死时使用回复一点体力，typeId 3
 class Tao extends Card{
+   public Tao(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative(){
       return true;
    }
@@ -59,6 +71,10 @@ class Tao extends Card{
 
 //酒：可在濒死时使用回复一点体力，或出牌阶段使用使下一张杀伤害加1（以此法每回合仅可使用一次）， typeId 4
 class Jiu extends Card{
+   public Jiu(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative(){
       return true;
    }
@@ -66,6 +82,10 @@ class Jiu extends Card{
 
 //顺手牵羊：可从距离为1的对手处拿走一张牌， typeId 5
 class ShunShouQianYang extends Card {
+   public ShunShouQianYang(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -77,6 +97,10 @@ class ShunShouQianYang extends Card {
 
 //过河拆桥：可弃置对手区域内一张牌，  typeId 6
 class GuoHeChaiQiao extends Card{
+   public GuoHeChaiQiao(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -88,6 +112,10 @@ class GuoHeChaiQiao extends Card{
 
 //无中生有：摸两张牌， typeId 7
 class WuZhongShengYou extends Card{
+   public WuZhongShengYou(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -95,6 +123,10 @@ class WuZhongShengYou extends Card{
 
 //借刀杀人：若对方装备武器，可指定其向另一人出杀，若不出杀，其将武器交给你， typeId 8
 class JieDaoShaRen extends Card{
+   public JieDaoShaRen(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -107,6 +139,10 @@ class JieDaoShaRen extends Card{
 
 //决斗：双方轮流出杀直到一人不出杀受到一点伤害， typeId 9
 class JueDou extends Card{
+   public JueDou(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -118,6 +154,10 @@ class JueDou extends Card{
 
 //无懈可击：可使敌方打出的锦囊牌无效， typeId 10
 class WuXieKeJi extends Card{
+   public WuXieKeJi(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -129,6 +169,10 @@ class WuXieKeJi extends Card{
 
 //乐不思蜀：置于敌方判定区，敌方摸牌前进行判定，有75%几率不能出牌， typeId 11
 class LeBuSiShu extends Card{
+   public LeBuSiShu(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -140,6 +184,10 @@ class LeBuSiShu extends Card{
 
 //兵粮寸断：置于敌方判定区，敌方摸牌前进行判定，有75%几率不能摸牌， typeId 12
 class BingLiangCunDuan extends Card{
+   public BingLiangCunDuan(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -151,6 +199,10 @@ class BingLiangCunDuan extends Card{
 
 //南蛮入侵：使用后其余所有角色需打出一张杀相应，否则受到一点伤害， typeId 13
 class NanManRuQin extends Card{
+   public NanManRuQin(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -158,6 +210,10 @@ class NanManRuQin extends Card{
 
 //万箭齐发：使用后其余所有角色需打出一张闪相应，否则受到一点伤害， typeId 14
 class WanJianQiFa extends Card{
+   public WanJianQiFa(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -165,6 +221,10 @@ class WanJianQiFa extends Card{
 
 //诸葛连弩；攻击距离为1，装备后使用杀无次数限制， typeId 15
 class ZhuGeLianNu extends Card{
+   public ZhuGeLianNu(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -172,6 +232,10 @@ class ZhuGeLianNu extends Card{
 
 //寒冰剑：攻击距离为2，装备后使用杀对敌方造成伤害时，可防止此伤害，改为弃置敌方2张牌， typeId 16
 class HanBingJian extends Card{
+   public HanBingJian(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -179,6 +243,10 @@ class HanBingJian extends Card{
 
 //古锭刀：攻击距离为2，装备后使用杀对敌方造成伤害时，若敌方无手牌，此伤害+1， typeId 17
 class GuDingDao extends Card{
+   public GuDingDao(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -186,6 +254,10 @@ class GuDingDao extends Card{
 
 //青龙偃月刀：攻击距离为3，装备后使用杀被闪响应后，可继续出杀， typeId 18
 class QingLongYanYueDao extends Card{
+   public QingLongYanYueDao(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -193,6 +265,10 @@ class QingLongYanYueDao extends Card{
 
 //丈八蛇矛：攻击距离为3，装备后可将任意两张牌当作杀使用或打出， typeId 19
 class ZhangBaSheMao extends Card{
+   public ZhangBaSheMao(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -200,6 +276,10 @@ class ZhangBaSheMao extends Card{
 
 //加1马：装备后别人计算与你的距离+1，typeId 20
 class HorseIncrease1 extends Card{
+   public HorseIncrease1(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
@@ -207,6 +287,10 @@ class HorseIncrease1 extends Card{
 
 //减1马：装备后别人计算与你的距离-1， typeId 21
 class HorseDecrease1 extends Card{
+   public HorseDecrease1(int typeId) {
+      super(typeId);
+   }
+
    public boolean CanInitiative() {
       return true;
    }
--- org/src/main/java/com/example/org/CardManager.java
+++ org/src/main/java/com/example/org/CardManager.java
@@ -1,4 +1,136 @@
 package com.example.org;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CardManager {
+    static List<Card> cardsPile = new ArrayList<>();
+    static Map<Integer,Card> cardsMap = new HashMap<>();
+    public static void init(){
+        Card card = new Sha(1);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new Shan(2);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new Tao(3);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new Jiu(4);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new ShunShouQianYang(5);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new GuoHeChaiQiao(6);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new WuZhongShengYou(7);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new JieDaoShaRen(8);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new JueDou(9);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new WuXieKeJi(10);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new LeBuSiShu(11);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new BingLiangCunDuan(12);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new NanManRuQin(13);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new WanJianQiFa(14);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new ZhuGeLianNu(15);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new HanBingJian(16);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new GuDingDao(17);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new QingLongYanYueDao(18);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new ZhangBaSheMao(19);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new HorseIncrease1(20);
+        cardsMap.put(card.getTypeId(),card);
+
+        card = new HorseDecrease1(21);
+        cardsMap.put(card.getTypeId(),card);
+
+        CreateCardList();
+    }
+    public static void CreateCardList(){
+        for(int i=0;i<30;i++){
+            cardsPile.add(new Card(1));
+        }
+        for(int i=0;i<15;i++){
+            cardsPile.add(new Card(2));
+        }
+        for(int i=0;i<8;i++){
+            cardsPile.add(new Card(3));
+        }
+        for(int i=0;i<5;i++){
+            cardsPile.add(new Card(4));
+        }
+        for(int i=0;i<5;i++){
+            cardsPile.add(new Card(5));
+        }
+        for(int i=0;i<6;i++){
+            cardsPile.add(new Card(6));
+        }
+        for(int i=0;i<4;i++){
+            cardsPile.add(new Card(7));
+        }
+        for(int i=0;i<2;i++){
+            cardsPile.add(new Card(8));
+        }
+        for(int i=0;i<3;i++){
+            cardsPile.add(new Card(9));
+        }
+        for(int i=0;i<3;i++){
+            cardsPile.add(new Card(10));
+        }
+        for(int i=0;i<3;i++){
+            cardsPile.add(new Card(11));
+        }
+        for(int i=0;i<2;i++){
+            cardsPile.add(new Card(12));
+        }
+        for(int i=0;i<3;i++){
+            cardsPile.add(new Card(13));
+        }
+        for(int i=0;i<1;i++){
+            cardsPile.add(new Card(14));
+        }
+        for(int i=0;i<2;i++){
+            cardsPile.add(new Card(15));
+        }
+        cardsPile.add(new Card(16));
+        cardsPile.add(new Card(17));
+        cardsPile.add(new Card(18));
+        cardsPile.add(new Card(19));
+        for(int i=0;i<2;i++){
+            cardsPile.add(new Card(20));
+        }
+        for(int i=0;i<2;i++){
+            cardsPile.add(new Card(21));
+        }
+
+    }
+
 }
