080d35c25a1bbe4492f4f1a8aca62ef779053e74
Gavin <149642813+Gavin-JN@users.noreply.github.com>
2024-09-05 13:39:12 +0800

    。

--- org/src/main/java/com/example/org/Heroes.java
+++ org/src/main/java/com/example/org/Heroes.java
@@ -41,7 +41,7 @@ class sunQuan extends Heroes {
     }
 
     //1、制衡。弃任意牌，摸等量牌，每回合一次
-    public void zhiheng(Player player,int discardCards){  //制衡（使用制衡之前要先判断玩家是否还有牌）{
+    public void zhiheng(Player player,int discardCards) {  //制衡（使用制衡之前要先判断玩家是否还有牌）{
         if (balanceUsed) {
             System.out.println("本回合制衡已经使用过了！");
             return;
@@ -52,27 +52,14 @@ class sunQuan extends Heroes {
         }
 
         // 弃牌并摸等量牌
-        player.setHandCardList();
-        // 摸等量牌  【摸排：从剩余卡堆中获得等量的卡牌数量加入到玩家目前已有的卡牌列表中】
-        player.DrawCard();
+//        player.setHandCardList();
+//        // 摸等量牌  【摸排：从剩余卡堆中获得等量的卡牌数量加入到玩家目前已有的卡牌列表中】
+//        player.DrawCard();
         balanceUsed = true;
         System.out.println("孙权使用了制衡，弃了" + discardCards + "张牌，摸了" + discardCards + "张牌。");
-
-    public Heroes(Player player) {
-        this.player = player;
-
     }
 
-    public Player getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-}
-
-class  caoCao extends Heroes {
+    public class  caoCao extends Heroes {
     //1、奸雄。获得对自己造成伤害的牌
     // (当角色收到伤害的时候调用该函数）
     public caoCao(){
@@ -113,14 +100,21 @@ class zhangFei extends Heroes {
     //咆哮——出牌阶段，你可以使用任意数量的【杀】。
     //该技能角色需主动选择使用
     //killCount 魏角色要出杀的数量
-    public void paoXiao(int killCount) {
-        if (getCards() >= killCount) {   //此处的getCards函数应改为牌中杀的数量
-            System.out.println("张飞使用了" + killCount + "张杀。");
-            setCards(getCards() - killCount);  // 扣除使用的杀
-        } else {
-            System.out.println("张飞没有足够的杀牌。");
-        }
+//    public void paoXiao(int killCount) {
+//        if (getCards() >= killCount) {   //此处的getCards函数应改为牌中杀的数量
+//            System.out.println("张飞使用了" + killCount + "张杀。");
+//            setCards(getCards() - killCount);  // 扣除使用的杀
+//        } else {
+//            System.out.println("张飞没有足够的杀牌。");
+//        }
+//    }
+
+    public void paoXiao()
+    {
+        //直接反馈一个信号，接触sha
     }
+    }
+}
 }
 
 class  zhuGeLiang extends Heroes {
@@ -145,22 +139,22 @@ class  zhangLiao extends Heroes {
         this.setHpLimit(4);
     }
     //突袭——摸牌阶段，你可以放弃摸牌，然后从至多两名（至少一名）角色的手牌里各抽取一张牌。◆摸牌阶段，你一旦发动突袭，就不能从牌堆获得牌；只剩一名其他角色时，你就只能选择这一名角色；若此时其他任何人都没有手牌，你就不能发动突袭。
-    public void tuXi(List<Heroes> players) {
-        int number=0;
-        for (Heroes player : players) {
-            if (player.getCards() > 0) {
-                player.setCards(player.getCards() - 1);  // 模拟从玩家手中抽一张牌
-                setCards(getCards() + 1);
-                System.out.println("张辽发动突袭，从对方手中抽了一张牌。");
-                number++;
-            } else {
-                System.out.println("目标玩家没有手牌。");
-            }
-            if(number==2) {  //至多抽取两名玩家的手牌
-                break;
-            }
-        }
-    }
+//    public void tuXi(List<Heroes> players) {
+//        int number=0;
+//        for (Heroes player : players) {
+//            if (player.getCards() > 0) {
+//                player.setCards(player.getCards() - 1);  // 模拟从玩家手中抽一张牌
+//                setCards(getCards() + 1);
+//                System.out.println("张辽发动突袭，从对方手中抽了一张牌。");
+//                number++;
+//            } else {
+//                System.out.println("目标玩家没有手牌。");
+//            }
+//            if(number==2) {  //至多抽取两名玩家的手牌
+//                break;
+//            }
+//        }
+  //  }
 }
 
 class daQiao extends Heroes {
@@ -171,7 +165,7 @@ class daQiao extends Heroes {
   //流离——当你成为【杀】的目标时，你可以弃一张牌，并将此【杀】转移给你攻击范围内的另一名角色。（该角色不得是【杀】的使用者）
   //当玩家被杀时，可自主选择是否使用该技能
     public void liuLi(){
-        setCards(getCards() -1);
+        //setCards(getCards() -1);
         //要判断是否处于攻击范围内
         //对另一名玩家实施杀
 
@@ -342,4 +336,4 @@ class  guoJia  extends Heroes {
 //   //指定两名玩家的手牌+1
 //  }
 //}
->>>>>>> 40086650255f8818e99091ae0fcf878691ba904d
+
