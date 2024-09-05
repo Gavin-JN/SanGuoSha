3c7b9b036946f7a437043f1e745039fab4742065
MagicSharp77 <149587362+MagicSharp77@users.noreply.github.com>
2024-09-05 14:05:57 +0800

    1211

--- org/src/main/java/com/example/org/client/Client.java
+++ org/src/main/java/com/example/org/client/Client.java
@@ -1,14 +1,42 @@
 package com.example.org.client;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("120.26.3.185",2777);//公网的ip地址，不在同一个局域网100.67.78.195
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) throws IOException {
+        //获取本机ip地址
+        InetAddress localhost = InetAddress.getLocalHost();
+        String ipAddress = localhost.getHostAddress();
+
+        //连接服务端，去找到指定服务器
+        Socket socket = new Socket("120.26.3.185", 8888);
+
+        //创建socket的输出流
+        OutputStream os = socket.getOutputStream();
+        //把低级的输出流包装成高级的数据输出流
+        DataOutputStream dos = new DataOutputStream(os);
+
+        //反复发消息
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("请说");
+            String msg = sc.nextLine();
+
+            if (msg.equals("exit")) {
+                System.out.println("欢迎下次使用");
+                dos.close();
+                socket.close();
+                break;
+            }
+            dos.writeUTF(ipAddress + ":" + msg);
+            dos.flush();
+
         }
+
     }
 }
