59d4f77c0d5e1718a336edf856cf66d0a0929255
MagicSharp77 <149587362+MagicSharp77@users.noreply.github.com>
2024-09-05 11:03:24 +0800

    9.5客户端和服务器端多收多发

--- org/src/main/java/com/example/org/server/GameServer.java
+++ org/src/main/java/com/example/org/server/GameServer.java
@@ -7,66 +7,25 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class GameServer implements Runnable {
-    private int port = 2777 ;//65535
-    private String host = "127.0.0.1" ;
-    private ServerSocket serverSocket ;//在net包上面，用来启动服务器
+public class GameServer{
+    //ServetSocket比Socket多的功能
+    //ServetSocket可以在服务器端创建端口号以对应链接，还可以处理Client发送的用户请求，发出等待请求
+    public static void main(String[] args) throws IOException {
 
-    public GameServer() {
-        try {
-            this.serverSocket = new ServerSocket(this.port);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }//默认端口
+        System.out.println("---服务端启动成功---");
+        //创建服务端Socket对象，同时注册端口
+        ServerSocket serverSocket = new ServerSocket(8888);
 
-    public GameServer(int port, String host) {
-        this.port = port;
-        this.host = host;
-        try {
-            this.serverSocket = new ServerSocket(this.port);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void start(){
+        //把这个客户端对应的通信管道交给一个独立的线程进行处理
         while(true){
-            Socket socket = null;
-            System.out.println("等待连接");
-            try {
-                socket = this.serverSocket.accept();    //这里会阻塞 要开一个新线程
-                System.out.println("已连接");
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//字节流和字符流
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                String host = socket.getInetAddress().getHostName();
-
-                User user =new User("",host,socket,in,out);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            //等待连接 这里accept返回值是一个socket对象
+            Socket socket =  serverSocket.accept();
+            //提示有人上线了
+            System.out.println("玩家上线:"+socket.getRemoteSocketAddress());
+            //启动一个新线程
+            new ServetReaderThread(socket).start();
         }
     }
 
 
-    public int getPort() {
-        return port;
-    }
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getHost() {
-        return host;
-    }
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    @Override
-    public void run() {
-
-    }
 }
