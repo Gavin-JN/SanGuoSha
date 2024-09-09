dd56505408a9cf9a703f37f24b3cd8ba1d16ce34
MagicSharp77 <149587362+MagicSharp77@users.noreply.github.com>
2024-09-09 18:31:33 +0800

    1212

--- org/src/main/java/com/example/org/controller/StartController.java
+++ org/src/main/java/com/example/org/controller/StartController.java
@@ -10,6 +10,7 @@ import com.example.org.fireWindow;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +34,17 @@ public class StartController extends Client{
 
     @FXML
     public void StartGame(ActionEvent event)throws IOException {
-        try (Socket socket = new Socket("192.168.185.82", 1688);
+        InetAddress localhost = InetAddress.getLocalHost();
+        String ipAddress = localhost.getHostAddress();
+
+        try (Socket socket = new Socket("192.168.58.82", 1688);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
 
             massage.put("NetCode", 1002);
             massage.put("count", 1);
+            massage.put("ip", ipAddress);
             String jsonString = massage.toString();
 
             // 发送消息到服务器
@@ -64,7 +69,8 @@ public class StartController extends Client{
 
 
         if(isPlayGame){
-            System.out.println("开始游戏了");
+
+            System.out.println(ipAddress+"开始游戏了");
             List<Player> players = new ArrayList<Player>();
 
             //玩家1
@@ -76,8 +82,51 @@ public class StartController extends Client{
             Room room=new Room(1);
             room.Init(players);
 
+
+            try (Socket socket = new Socket("192.168.58.82", 1688);
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+            ) {
+
+                // 发送消息到服务器
+                massage.put("NetCode", 1010);
+                String jsonString = massage.toString();
+                out.println(jsonString);
+                massage.clear();
+
+                //设置回复信息0
+                massage.put("Order",0);
+                String jsonString0 = massage.toString();
+                massage.clear();
+
+                //设置回复信息1
+                massage.put("Order",1);
+                String jsonString1 = massage.toString();
+                massage.clear();
+
+                // 读取并打印服务器的响应
+                String response;
+                while ((response = in.readLine()) != null) {
+                    if(response.equals(jsonString0)||response.equals(jsonString1)) {
+                        System.out.println(response);
+                        break;
+                    }
+                }
+                jsonString = response;
+
+
+                out.close();
+                in.close();
+                socket.close();
+                isPlayGame = true;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             fireWindow player=new fireWindow(players.get(0),players.get(1));  //传入两个玩家
 
+            we.ip=ipAddress;
 
         }
     }
--- org/src/main/java/com/example/org/server/GameServer.java
+++ org/src/main/java/com/example/org/server/GameServer.java
@@ -1,11 +1,16 @@
 package com.example.org.server;
 
+import com.example.org.Player;
+import com.example.org.fireWindow;
 import com.example.org.server.impl.GameEventHandlingImpl;
 import org.json.JSONObject;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,43 +27,102 @@ public class GameServer {
     //信号处理者
     public static GameEventHandlingImpl gameEventHandling = new GameEventHandlingImpl();
 
-    //游戏玩家
+    //游戏玩家数
     public static int count = 0;
 
+    public static int number = 0;
+
+    //游戏玩家的ip
+    public static String player0_ip;
+    public static String player1_ip;
+
+    //线程集存储所有的客户端
+    private static final ConcurrentHashMap<Socket, PrintWriter> clientMap = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Integer, PrintWriter> clientMap_everyone = new ConcurrentHashMap<>();
+
+    private static int clientIdCounter = 0;  // 用于分配唯一的客户端ID
+    private static String[] clientIpMap = new String[100];
 
 
-
-    //ServetSocket比Socket多的功能
-    //ServetSocket可以在服务器端创建端口号以对应链接，还可以处理Client发送的用户请求，发出等待请求
     public static void main(String[] args) throws IOException {
+        //线程集初始化
+        clientMap.clear();
+        clientMap_everyone.clear();
+        clientIdCounter = 0;
+        //服务器启动提示
         System.out.println("---服务端启动成功---");
         //创建服务端Socket对象，同时注册端口
         ServerSocket serverSocket = new ServerSocket(1688);
-        //线程池 用于统一发信息
-        ExecutorService threadPool = Executors.newFixedThreadPool(10);
-
 
         //把这个客户端对应的通信管道交给一个独立的线程进行处理
         while (true) {
-            //等待连接 这里accept返回值是一个socket对象
-            Socket socket = serverSocket.accept();
-            //提示有人上线了
-            System.out.println("玩家上线:" + socket.getRemoteSocketAddress());
-            //启动一个新线程
-//            GameServetReaderThread readerThread = new GameServetReaderThread(socket);
-            threadPool.submit(new ClientHandler(socket));
-//            Thread thread = new Thread(readerThread);
-//            thread.start();
+            //连接客户端
+            Socket clientSocket = serverSocket.accept();
+            //获取分配id
+            int clientId = clientIdCounter++;
+            clientIpMap[clientId] = clientSocket.getInetAddress().getHostAddress();
+            clientMap_everyone.put(clientId, new PrintWriter(clientSocket.getOutputStream(), true));
+
+            new Thread(new ClientHandler(clientSocket,clientId,clientIpMap[clientId])).start();
+
+//            massage.put("Order",0);
+//            String jsonString = massage.toString();
+//            sendMessageToClient(0,jsonString);
+//            massage.clear();
+//
+//            massage.put("Order",1);
+//            jsonString = massage.toString();
+//            sendMessageToClient(1,jsonString);
+//
+//            massage.put("Order",2);
+//            jsonString = massage.toString();
+//            sendMessageToClient(2,jsonString);
+//
+//            massage.put("Order",3);
+//            jsonString = massage.toString();
+//            sendMessageToClient(3,jsonString);
+//
+//            massage.put("Order",4);
+//            jsonString = massage.toString();
+//            sendMessageToClient(4,jsonString);
+
+            //打印一下是否会存储到客户端的ip地址
+            PrintWriter writer = clientMap_everyone.get(0);
+            System.out.println("Client ID: " + 0 + ", PrintWriter: " + writer);
         }
     }
 
-
-    private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
-
-        public ClientHandler(Socket socket) {
-            this.clientSocket = socket;
+    //统一发信息
+    public static void broadcastMessage(String message) {
+        for (PrintWriter writer : clientMap.values()) {
+            writer.println(message);
         }
+    }
+
+    //给特定客户端发信息
+    public static void sendMessageToClient(int clientId, String message) {
+        PrintWriter writer = clientMap_everyone.get(clientId);
+        if (writer != null) {
+            writer.println(message);
+        }
+    }
+
+    //初始化单例客户端
+    private static class ClientHandler implements Runnable {
+        //客户端的Socket节点
+        private final Socket clientSocket;
+        //客户端在线程集中的编号
+        private int clientId;
+        //客户端在线程集中的ip地址记录
+        private String clientIp;
+
+        //构造器
+        public ClientHandler(Socket socket, int clientId,String clientIp) {
+            this.clientSocket = socket;
+            this.clientId = clientId;
+            this.clientIp = clientIp;
+        }
+
 
         @Override
         public void run() {
@@ -66,10 +130,17 @@ public class GameServer {
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
             ) {
+                clientMap.put(clientSocket, out);
 
                 String jsonString = in.readLine();
                 massage = new JSONObject(jsonString);
                 netCode = massage.getInt("NetCode");
+                System.out.println(massage.getString("ip")+"：加入游戏");
+                if(player0_ip!=null){
+                    player0_ip = massage.getString("ip");
+                }else {
+                    player1_ip = massage.getString("ip");
+                }
 
                 switch(netCode){
                     case 1002:
@@ -82,27 +153,63 @@ public class GameServer {
                         while(true){
                             if(count<2){
                                 out.println("0");
-                                System.out.println(count);
+//                                System.out.println(count);
                             }else {
-                                System.out.println(count);
+//                                System.out.println(count);
                                 count = 0;
                                 System.out.println("游戏开始");
                                 // 发送回消息或处理其他逻辑
-                                out.println("1");
+                                // 1表示开始游戏
+                                broadcastMessage("1");
                                 out.close();
                                 in.close();
                                 clientSocket.close();
                                 break;
                             }
-
                         }
                         break;
                     case 1010:    //第二个信息
+                    {
+
+
+                        Player player0 =new Player();
+                        Player player1 =new Player();
+                        List<Player> players = new ArrayList<Player>();
+                        players.add(player0);
+                        players.add(player1);
+                        if(players.get(0).seatId==0)
+                        {
+                            players.get(1).setSeatId(1);
+                        }
+                        else {
+                            players.get(1).setSeatId(0);
+                        }
+
+                        player0.ip = player0_ip;
+                        player1.ip = player1_ip;
+//                        PrintWriter writer = clientMap_everyone.get(2);
+//                        System.out.println("Client ID: " + 2 + ", PrintWriter: " + writer);
+
+                        massage.put("Order",0);
+                       jsonString = massage.toString();
+                        sendMessageToClient(2,jsonString);
+                        massage.clear();
+
+                        massage.put("Order",1);
+                        jsonString = massage.toString();
+                        sendMessageToClient(3,jsonString);
+                        massage.clear();
+
+//                        //随机发 0_ip的是0
+//                        //1_ip的是1
+                    }
+
 
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
+                clientMap.remove(clientSocket);
                 try {
                     clientSocket.close();
                 } catch (IOException e) {
